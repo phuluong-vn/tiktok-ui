af0f928e00276478d67f6006f5ad495815de761f
phuluong <phuluong2019@gmail.com>
2023-10-11 00:13:09 +0700

    Xây dựng UI phần Header #1

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,14 +1,28 @@
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss' 
 import images  from '~/assets/images';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass, faSpinner, faCircleXmark } from '@fortawesome/free-solid-svg-icons';
 
 const cx = classNames.bind(styles)
 function Header() {
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
-                <div className={cx('logo')}>
                     <img src={images.logo} alt='logo-tiktok'/>
+                <div className={cx('seach')}>
+                    <input placeholder='Seach accounts and videos' spellCheck={false}/>
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                    <button className={cx('seach-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                <div className={cx('action')}>
+                    <button>Register</button>
+                    <button>Login</button>
                 </div>
             </div>
         </header>
